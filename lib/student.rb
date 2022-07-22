944fc343947aee0a2fe80acfdd50935f82a31738
Joseph Hirotsu <jhirotsu@gmail.com>
2022-07-22 13:36:12 -0400

    commit tree f16a3e83ae2748fd88373f92308e3b00de55a96c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
+require 'pry'
 
-end
+class Student < User
+    attr_reader :knowledge, :first_name, :last_name
+
+    #initialize student with knowledge points to open array
+    def initialize
+        @knowledge = []
+        @first_name = ""
+        @last_name = ""
+      end
+    def first_name(name)
+        @first_name=name
+    end
+    def last_name(name)
+        @last_name= name
+    end
+
+    #define method #learn that takes a string and adds to @knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+      end
+    
+      student = self.new
+binding.pry
+
+  end
+  
